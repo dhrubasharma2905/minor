2d8f1a082596954e65db1dffd6e8878f8e9e8f03
dhrubasharma2905 <dhrubasharma41@gmail.com>
2022-05-06 09:07:18 +0545

    third commit

--- Controller/usercontrol.js
+++ Controller/usercontrol.js
@@ -1,8 +1,5 @@
 const User = require('../Models/userregister')
-const Token = require('../Models/tokenmodel')
-const sendEmail = require('../utlis/setemail')
 const jwt = require('jsonwebtoken')
-const expressjwt = require('express-jwt')
 exports.userregisters = async(req,res) =>
 {   
     try{   
@@ -16,77 +13,32 @@ exports.userregisters = async(req,res) =>
             user.images=req.files.map(f=>({url:f.path,filename:f.filename}));
             user = await user.save();
             if(!user){return res.status(400).json({error:"user not found"})}
-            // let token = new Token(
-            //     {
-            //         token:crypto.randomBytes(16).toString('hex'),
-            //         userId:user._id
-            //     })
-            // token = await token.save()
-            // if(!token) { return res.status(400).json({error:"not found the token"})}
-            // sendEmail({
-            //     from:'newreply@ecommerce-api.com',
-            //     to:user.email,
-            //     subject:'Email Verification Link',
-            //     text:`Hello,\n\n verify your email by clicking inthe link below:\n\n http:\/\/${req.headers.host}\/api/confirmation\/${token.token}`,
-            //     //http://localhost:5000/api/confimation/abde245342d
-            // })
             else res.send(user)
         }
     } catch (e) {
           return res.status(404).json({ err: e.message });
 }
 }
-// confirmation of email
-// exports.postEmailConfirmation = (req,res) =>
-// {
-//     // suru ma token find garna paryo
-//     Token.findOne({token:req.params.token},(error,token)=>{
-//         if(error || !token){
-//             return res.status(400).json({error:"invalid token or token may have expired"})
-//         }
-//     User.findOne({_id:token.userId},(error,user)=>{
-//         if(error || !user){
-//             return res.status(400).json({error:"sorry we are unable to find the valid user for this token"})
-//         }
-    
-//     // if user is already verified then
-//     if(user.isVerified)
-//     {
-//         return res.status(400).json({error:`email is already verified , login to continure`})
-//     }
-//     user.isVerified=true
-//     user.save();
-//     if(error){return res.status(400).json({error:error})}
-//     res.json({msg:"congrats your email has been verified"})
-// })
-// })}
-// signin process
 exports.signIn = async(req,res)=>{
-    console.log(req.body)
-    const {email,password} = req.body
-
-    // const email = req.body.email
-    // const password = req.body.password
-    // at first check if email is registerd or not
-    const user = await User.findOne({email})
+    
+    const email =req.body.email
+    const originalpassword = req.body.password
+    const user = await User.findOne({email:email})
     if(!user) {return res.status(403).json({error:`sorry the email is not found`})}
-    // if email is found then check the matching password for that
-    if(!user.authenticate(password))
+    if(!user.authenticate(originalpassword))
     {
         return res.status(400).json({error:`email and password doesnot match`})
     }
-    // check if user is verified or not 
-    // if(!user.isVerified){
-    //     return res.status(400).json({error:`verify your email to continue`})
-    // }
-    const token = jwt.sign({_id:user._id},process.env.JWT_SECRET) // secret or private key should have value 
-    // but not the string otherwise it will give error
-    res.cookie('hellocookie',token,{expire:Date.now()+999999})
+    const accesstoken = jwt.sign({
+        id:user._id,
+        role:user.role
+    },process.env.JWT_SECRET,{expiresIn:'1d'});
 
     // return user infromation to frontend
-    const{_id,name,role}=user
-    return res.json({token,user:{name,email,_id,role}})
+    const{password,...others}=user._doc
+    return res.json({accesstoken,...others})
 }
+
 exports.signout=(req,res) =>
 {
     res.clearCookie('hellocookie')
@@ -95,7 +47,7 @@ exports.signout=(req,res) =>
 // userlist
 exports.userlist=async (req,res)=>
 {
-    const user = await User.find().select('-hash_password')
+    const user = await User.find(req.params.id).select('-hash_password')
     if(!user)
     {
         return res.status(400).json({error:"user not found"})
@@ -105,29 +57,8 @@ exports.userlist=async (req,res)=>
 }
 exports.singleuser = async(req,res) =>
 {
-    const user = User.findById(req.params.id).select('-hash_password')
+    const user = await User.findById(req.params.id).select('-hash_password')
     if(!user){return res.status(400).json({error:"the user is not found"})}
     res.send(user)
 }
-// resend email verification link
-// exports.resendverificationemail = async(req,res) =>
-// {
-//     let user = await User.findOne({email:req.body.email})
-//     if(!user){return res.status(400).json({error:"please register the email"})}
-//     if(user.isVerified){return res.status(400).json({error:"the email is already verifed , login to continue"})}
-//     // create token to store in database and send the verification link
-//     let token = new Token({
-//         token:crypto.randomBytes(16).toString('hex'),
-//         userId:user._id
-//     })
-//     token = await token.save()
-//     if(!token){return res.status(400).json({error:"token not found"})}
-//     sendEmail({
-//         from:'newreply@ecommerce-api.com',
-//         to:user.email,
-//         subject:'Email Verification Link',
-//         text:`Hello,\n\n verify your email by clicking inthe link below:\n\n http:\/\/${req.headers.host}\/api/confirmation\/${token.token}`,
-//         //http://localhost:5000/api/confimation/abde245342d
-//     })
-//     res.json({message:`verification link has been sent`})
-// }
+
---
+++ middleware/verifytoken.js
@@ -0,0 +1,39 @@
+const jwt = require("jsonwebtoken")
+const verifytoken = (req,res,next)=>
+{
+    const authHeader = req.headers.token;
+    if(authHeader)
+    {
+        const settoken = authHeader.split(' ')[1]
+        jwt.verify(settoken,process.env.JWT_SECRET,(err,user)=>
+        {
+            if(err) {res.status(403).json({error:"invalid token"})}
+            req.user=user;
+            next()
+        })
+    } else {
+        return res.status(401).json({error:"You are not authenticated"})
+    }
+}
+const verifyTokenAndAuthorization = (req,res,next)=>
+{
+    verifytoken(req,res,()=>
+    {
+        if(req.user.id===req.params.id || req.user.role){
+            next()
+        }else{return res.status(403).json({error:"you are not allowed to do that!"})}
+    })
+}
+const verifyTokenAndAdmin = (req,res,next)=>
+{
+    verifytoken(req,res,()=>
+    {
+        if(req.user.role)
+        {
+            next()
+        }else {return res.status(403).json({error:"You are not allowed to do that"})}
+    })
+}
+
+
+module.exports={verifytoken,verifyTokenAndAuthorization,verifyTokenAndAdmin}
